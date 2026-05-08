8db58abe5fd02d72280dcb732112f9db62980119
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-05-08 19:53:38 +0530

    readme added

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HomePage from "./HomePage";
 
 export default function Page() {
   return (
+    // homepage
     <Suspense fallback={<div>Loading...</div>}>
       <HomePage />
     </Suspense>
