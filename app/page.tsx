7145fc7dabef14552c3ebc1ec387d69af8972c62
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-05-26 23:47:49 +0530

    indentation and naming

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Suspense } from "react";
-import HomePage from "./HomePage";
 import Component from "../components/Component";
 
 export default function Page() {
   return (
+
+
+    // 
       <Component />
   );
 }
