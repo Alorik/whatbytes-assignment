cdfdf698acf1317d0e0335d6297b27dcf49330ae
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-07-08 10:32:28 +0530

    fix: suspense added to prevent csr

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 
-import HomePage from "./HomePage";
+import { Suspense } from "react";
+import HomePage from "../components/HomePage";
 
 export default function Page() {
   return (
-<HomePage />
+    <Suspense fallback={<div>Loading....</div>}>
+      <HomePage />
+    </Suspense>
   );
 }
